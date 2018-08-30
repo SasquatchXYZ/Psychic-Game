3c7df10da74f420b3a13f50c0a0e92d4a12d3381
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-08-30 00:48:59 -0400

    Completed JS and working HTML

---
+++ assets/javascript/game.js
@@ -0,0 +1,73 @@
+// Creating Global variables to hold the number of Wins, Losses, Lives, an array to hold guessedLetters,
+// the Computer's letter selection, and obviously the most important... an array singing the alphabet.
+var wins = 0;
+var losses = 0;
+var lives = 9;
+var guessedLetters = [];
+var computerChoice;
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// Creating variables that hold the references to the location in the HTML where we want to display things.
+var winsTally = document.getElementById("wins-tally");
+var lossesTally = document.getElementById("losses-tally");
+var guessesLeft = document.getElementById("guesses-left");
+var userGuessLog = document.getElementById("userGuesses-log");
+
+// FUNCTIONS
+//======================================================================================================================
+
+function gameStart() {
+    winsTally.textContent = "Wins: " + wins;
+    lossesTally.textContent = "Losses: " + losses;
+    guessesLeft.textContent = "Guesses Remaining: " + lives;
+    userGuessLog.textContent = "Guessed Letters: " + guessedLetters;
+   // computerChoice = alphabet[Math.floor(Math.random() * 26)];
+   // console.log(computerChoice);
+}
+
+function gameReset() {
+    lives = 9;
+    guessedLetters = [];
+    computerChoice = alphabet[Math.floor(Math.random() * 26)];
+    console.log(computerChoice);
+}
+
+// MAIN PROCESS
+//======================================================================================================================
+gameReset();
+gameStart();
+//console.log(losses);
+
+document.onkeyup = function(event) {
+    var userGuess = event.key.toLowerCase();
+
+   // var computerChoice = alphabet[Math.floor(Math.random() * 26)];
+   // console.log(computerChoice);
+if (alphabet.indexOf(userGuess) >= 0) {
+    if (userGuess === computerChoice) {
+        wins++;
+        gameReset();
+    }
+    else {
+        lives--;
+        guessedLetters.unshift(" " + userGuess.toUpperCase() + " ");
+        console.log(guessedLetters);
+    }
+    if (lives === 0) {
+        losses++;
+        gameReset();
+    }
+    gameStart();
+}
+else {
+    alert("That is not a letter... Psychics don't like tricksters...");
+    gameReset();
+    gameStart();
+}
+   // winsTally.textContent = "Wins: " + wins;
+   // guessesLeft.textContent = "Guesses Remaining: " + lives;
+   // userGuessLog.textContent = "Guessed Letters: " + guessedLetters;
+};
+
+
+//console.log(alphabet[Math.floor(Math.random() * 26)]);
